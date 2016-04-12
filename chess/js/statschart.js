0489f338852aad11c5aa10dd1332159596f5cd50
Adrien <adrien.lafeuille@gmail.com>
2016-04-12 23:12:35 +0200

    Update statschart.js

--- chess/js/statschart.js
+++ chess/js/statschart.js
@@ -11,7 +11,7 @@ var mydata = [['Turn', 'Rank'],[0, 0],];
 function drawChart() 
 {
 var data = google.visualization.arrayToDataTable(mydata, false); // 'false' means that the first row contains labels, not data.
-var options = {pointSize: 5, colors: ['black'], width: 340, height: 350, legend: 'none', chartArea: {'width': '80%', 'height': '80%'},vAxis: {format:'#'}, hAxis: {format:'#'}};
+var options = {hAxis: {title: 'Moves'}, vAxis: {title: 'Board balance'}, pointSize: 5, colors: ['black'], width: 340, height: 350, legend: 'none', chartArea: {'width': '80%', 'height': '80%'},vAxis: {format:'#'}, hAxis: {format:'#'}};
 var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
 chart.draw(data, options);
 };
