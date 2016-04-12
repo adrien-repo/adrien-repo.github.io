62279ec140b25141c6f03f2358aacdc46dacf948
Adrien <adrien.lafeuille@gmail.com>
2016-04-12 17:06:32 +0200

    Update listallmoves.js

--- chess/js/listallmoves.js
+++ chess/js/listallmoves.js
@@ -22,7 +22,7 @@ function listlegalmoves()
 					legalmoveslist = legalmoveslist.concat([[mylegco,mylegli,mylegco-2,mylegli]])
 					}
 				}
-				//can eat blavk material (<0) in diagonal 1 cell forward, to the left
+				//can eat black material (<0) in diagonal 1 cell forward, to the left
 				if (matrix[mylegco-1][mylegli-1] < 0)
 				{
 					legalmoveslist = legalmoveslist.concat([[mylegco,mylegli,mylegco-1,mylegli-1]])
@@ -33,7 +33,69 @@ function listlegalmoves()
 					legalmoveslist = legalmoveslist.concat([[mylegco,mylegli,mylegco-1,mylegli+1]])
 				}
 			}
-	
+			//white bishop
+			if (mylegstartcell == value_bishop)
+			{
+				//up left (- -)
+				var cangoahead = 1
+				for(var mylegbishop=1; mylegbishop<8; mylegbishop++) 
+				{
+					if ((matrix[mylegco-mylegbishop][mylegli-mylegbishop] == 0) && (cangoahead == 1))
+					{
+						legalmoveslist = legalmoveslist.concat([[mylegco,mylegli,mylegco-mylegbishop,mylegli-mylegbishop]])
+					}
+					if ((matrix[mylegco-mylegbishop][mylegli-mylegbishop] < 0) && (cangoahead == 1))
+					{
+						legalmoveslist = legalmoveslist.concat([[mylegco,mylegli,mylegco-mylegbishop,mylegli-mylegbishop]])
+						cangoahead = 0
+					}
+				}
+				//up right (- +)
+				var cangoahead = 1
+				for(var mylegbishop=1; mylegbishop<8; mylegbishop++) 
+				{
+					if ((matrix[mylegco-mylegbishop][mylegli+mylegbishop] == 0) && (cangoahead == 1))
+					{
+						legalmoveslist = legalmoveslist.concat([[mylegco,mylegli,mylegco-mylegbishop,mylegli+mylegbishop]])
+					}
+					if ((matrix[mylegco-mylegbishop][mylegli+mylegbishop] < 0) && (cangoahead == 1))
+					{
+						legalmoveslist = legalmoveslist.concat([[mylegco,mylegli,mylegco-mylegbishop,mylegli+mylegbishop]])
+						cangoahead = 0
+					}
+				}
+				//down left (+ -)
+				var cangoahead = 1
+				for(var mylegbishop=1; mylegbishop<8; mylegbishop++) 
+				{
+					if ((matrix[mylegco+mylegbishop][mylegli-mylegbishop] == 0) && (cangoahead == 1))
+					{
+						legalmoveslist = legalmoveslist.concat([[mylegco,mylegli,mylegco+mylegbishop,mylegli-mylegbishop]])
+					}
+					if ((matrix[mylegco+mylegbishop][mylegli-mylegbishop] < 0) && (cangoahead == 1))
+					{
+						legalmoveslist = legalmoveslist.concat([[mylegco,mylegli,mylegco+mylegbishop,mylegli-mylegbishop]])
+						cangoahead = 0
+					}
+				}
+				//down right (+ +)
+				var cangoahead = 1
+				for(var mylegbishop=1; mylegbishop<8; mylegbishop++) 
+				{
+					if ((matrix[mylegco+mylegbishop][mylegli+mylegbishop] == 0) && (cangoahead == 1))
+					{
+						legalmoveslist = legalmoveslist.concat([[mylegco,mylegli,mylegco+mylegbishop,mylegli+mylegbishop]])
+					}
+					if ((matrix[mylegco+mylegbishop][mylegli+mylegbishop] < 0) && (cangoahead == 1))
+					{
+						legalmoveslist = legalmoveslist.concat([[mylegco,mylegli,mylegco+mylegbishop,mylegli+mylegbishop]])
+						cangoahead = 0
+					}
+				}
+
+
+
+			}
 	
 	}
 	}
