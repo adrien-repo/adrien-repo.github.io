ed4910c2f71b818ac0e521c79d83258e0ec9fbc1
Adrien <adrien.lafeuille@gmail.com>
2016-04-12 18:15:49 +0200

    Update listallmoves.js

--- chess/js/listallmoves.js
+++ chess/js/listallmoves.js
@@ -61,7 +61,7 @@ function listlegalmoves()
 				}
 				//up right (- +)
 				cangoahead = 1
-				alert('up left:'+Math.min(7-mylegco,7-mylegli))
+				alert('up left:'+Math.max(7-mylegco,7-mylegli))
 				for (var mylegbishop=1; mylegbishop<=(Math.max(7-mylegco,7-mylegli)); mylegbishop++) 
 				{
 					if ((matrix[mylegco-mylegbishop][mylegli+mylegbishop] == 0) && (cangoahead == 1))
