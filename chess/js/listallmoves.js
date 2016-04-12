c44fca2a23ee3638afa3462a9f1e13eb974aa98e
Adrien <adrien.lafeuille@gmail.com>
2016-04-12 16:38:19 +0200

    Update listallmoves.js

--- chess/js/listallmoves.js
+++ chess/js/listallmoves.js
@@ -16,11 +16,21 @@ function listlegalmoves()
 				if (matrix[mylegco-1][mylegli] == 0)
 				{
 				legalmoveslist = legalmoveslist.concat([[mylegco,mylegli,mylegco-1,mylegli]])
-					//can go 2 cells fwd if both are empty and is first pawn move
+					//and can go 2 cells fwd if both are empty and is first pawn move
 					if ((matrix[mylegco-2][mylegli] == 0) && (mylegco == 6))
 					{
 					legalmoveslist = legalmoveslist.concat([[mylegco,mylegli,mylegco-2,mylegli]])
 					}
+				}
+				//can eat blavk material (<0) in diagonal 1 cell forward, to the left
+				if (matrix[mylegco-1][mylegli-1] < 0)
+				{
+					legalmoveslist = legalmoveslist.concat([[mylegco,mylegli,mylegco-1,mylegli-1]])
+				}
+				//can eat black material (<0) in diagonal 1 cell forward, to the right
+				if (matrix[mylegco-1][mylegli+1] < 0)
+				{
+					legalmoveslist = legalmoveslist.concat([[mylegco,mylegli,mylegco-1,mylegli+1]])
 				}
 			}
 	
