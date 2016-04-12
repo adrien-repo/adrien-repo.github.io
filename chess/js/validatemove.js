94135ad1460b1a441d11e1151606537c14548bc5
Adrien <adrien.lafeuille@gmail.com>
2016-04-12 15:34:30 +0200

    Added files via upload

---
+++ chess/js/validatemove.js
@@ -0,0 +1,32 @@
+function validatemove()
+{
+	listlegalmoves();
+	var passedtest = 0;
+	var candidatemove = [];
+	candidatemove = candidatemove.concat([[starty,startx,stopy,stopx]])
+	
+	for (lilegal = 0; lilegal < legalmoveslist.length; lilegal++) 
+	{ 
+		if (legalmoveslist[lilegal].equals(candidatemove[0])) 
+			{
+			passedtest = 1
+			}
+	}
+	
+	if (passedtest == 1)
+	{
+		//make move in matrix and render
+		matrix[stopy][stopx] = matrix[starty][startx]
+		matrix[starty][startx] = 0
+		drawboard();
+		drawmaterial();
+		//update chart for stats
+		var currentrank = (matrix.reduce(function(a,b) { return a.concat(b) }).reduce(function(a,b) { return a + b })).toFixed(2)
+		mydata.push([myturn,currentrank])
+		myturn=myturn+1;
+		drawChart();
+		document.getElementById('infotext').innerHTML = "< />";
+	} else {
+		document.getElementById('infotext').innerHTML = "< Move not allowed ! />";
+	}
+};	
