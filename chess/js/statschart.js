8603caf7a26298a71342a4f178df20412aa64ad1
Adrien <adrien.lafeuille@gmail.com>
2016-04-12 12:19:05 +0200

    Update statschart.js

--- chess/js/statschart.js
+++ chess/js/statschart.js
@@ -1,1 +1,17 @@
-a
+$( "#showhidestats" ).click(function() {
+$( "#chart_div" ).toggle();
+$( "#board_div" ).toggle();
+});
+
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(drawChart);
+var myturn = 1
+var mydata = [['Turn', 'Rank'],[0, 0],];
+
+function drawChart() 
+{
+var data = google.visualization.arrayToDataTable(mydata, false); // 'false' means that the first row contains labels, not data.
+var options = {pointSize: 5, colors: ['black'], width: 260, height: 270, legend: 'none', chartArea: {'width': '80%', 'height': '80%'},vAxis: {format:'#'}, hAxis: {format:'#'}};
+var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+chart.draw(data, options);
+};
