59bffc7dceaac9c996f9309ac495d789b744d6f9
Adrien <adrien.lafeuille@gmail.com>
2016-04-22 23:41:09 +0200

    Update aiturn-dev.js

--- chess/js/aiturn-dev.js
+++ chess/js/aiturn-dev.js
@@ -94,7 +94,35 @@ function aiturn()
     utility_human = evaluate_second
     
     
-    
+    //THIRD AI MOVE
+    //3.0 - for a given player color and matrix
+    whoisthinking = -1
+    //we use matrix_second as start point
+    //3.1 - list all legal moves + clear chess-resulting prohibited moves (external fucntions)
+    list_third = listlegalmoves(matrix_second);
+    list_third = removechessfromlist(list_third,matrix_second);
+    //2.2 - loop all admitted moves, get board rank <reloop here if needed>
+    //creat blank matrix
+    var matrix_third = new Array(8);
+    for(var i=0; i<8; i++) {matrix_third[i] = new Array(8);}
+    for(var myzeroli=0; myzeroli<8; myzeroli++) {for(var myzeroco=0; myzeroco<8; myzeroco++) {matrix_third[myzeroco][myzeroli] = 0}}
+    //loop admitted moves
+    for (li_third = 0; li_third < list_third.length; li_third++) 
+        { 
+        //get 4 coordinates of move candidate
+        starty_third = list_third[li_third][0]
+        startx_third = list_third[li_third][1]
+        stopy_third = list_third[li_third][2]
+        stopx_third = list_third[li_third][3]
+        //set blank matrix to current board state
+            for(var myzeroli=0; myzeroli<8; myzeroli++)
+                {for(var myzeroco=0; myzeroco<8; myzeroco++) 
+                    {matrix_third[myzeroco][myzeroli] = matrix_second[myzeroco][myzeroli]}}
+        //simulate admitted move in mirrored matrix
+        matrix_third[stopy_third][stopx_third] = matrix_third[starty_third][startx_third]
+        matrix_third[starty_third][startx_third] = 0
+        //evaluate board after move
+        evaluate_third = (matrix_third.reduce(function(a,b) { return a.concat(b) }).reduce(function(a,b) { return a + b })).toFixed(2)
     
     
     
@@ -102,8 +130,10 @@ function aiturn()
     
         utility_first = evaluate_first - evaluate_zero
         utility_second = evaluate_second - evaluate_first
+        utility_third = evaluate_third - evaluate_second
+
     
-        utility_function = utility_first + utility_second
+        utility_function = utility_first + utility_second + utility_third
         //keep the move that minimizes the utility_function
         if (utility_function < utility_treshold)
         {
@@ -117,7 +147,8 @@ function aiturn()
 
         }
 
-        
+    //end of third loop (for)
+    }
 
     //end of best human (if)
     }
