3955248bd236ff136b80a8dd4e1e8f206ecd48fa
Adrien <adrien.lafeuille@gmail.com>
2016-04-23 00:52:35 +0200

    Update aiturn-dev.js

--- chess/js/aiturn-dev.js
+++ chess/js/aiturn-dev.js
@@ -151,8 +151,7 @@ function aiturn()
     for (li_fourth = 0; li_fourth < list_fourth.length; li_fourth++) 
         { 
         nbtests = nbtests+1
-            document.getElementById('txtbox').value = "Thinking..(best: "+utility_treshold+" / "+nbtests+")";
-            window.status="Thinking..(best: "+utility_treshold+" / "+nbtests+")";  
+            document.getElementById('txtbox').innerHTML = "Thinking..(best: "+utility_treshold+" / "+nbtests+")";
 
         //get 4 coordinates of move candidate
         starty_fourth = list_fourth[li_fourth][0]
