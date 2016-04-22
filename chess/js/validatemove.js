c973838459d01f1d8f99af9ec3ef0070582f183e
Adrien <adrien.lafeuille@gmail.com>
2016-04-22 11:37:58 +0200

    Update validatemove.js

--- chess/js/validatemove.js
+++ chess/js/validatemove.js
@@ -1,5 +1,8 @@
 function validatemove()
 {
+	
+if (whoisthinking == 1)
+{
 	listlegalmoves();
 	var passedtest = 0;
 	var candidatemove = [];
@@ -18,6 +21,30 @@ function validatemove()
 		//make move in matrix and render
 		matrix[stopy][stopx] = matrix[starty][startx]
 		matrix[starty][startx] = 0
+		//drawboard();
+		//drawmaterial();
+		
+		//update chart for stats
+		var currentrank = (matrix.reduce(function(a,b) { return a.concat(b) }).reduce(function(a,b) { return a + b })).toFixed(2)
+		mydata.push([myturn,currentrank])
+		myturn=myturn+1;
+		drawChart();
+		document.getElementById('infotext').innerHTML = "You have moved ! Now I am thinking..";
+		//AI turn now !
+		whoisthinking = -1;
+
+		try {
+		aiturn();
+		}
+		catch(err) {alert(err.message)
+			}
+		//need to store back choosen move in startx, etc values for move tb drawed with highlight
+		starty = startychoosen;
+		startx = startxchoosen;
+		stopy = stopychoosen;
+		stopx = stopxchoosen;
+		matrix[stopy][stopx] = matrix[starty][startx]
+		matrix[starty][startx] = 0
 		drawboard();
 		drawmaterial();
 		//update chart for stats
@@ -26,7 +53,13 @@ function validatemove()
 		myturn=myturn+1;
 		drawChart();
 		document.getElementById('infotext').innerHTML = "Your turn !";
+		//Human turn now !
+		whoisthinking = 1;
+		
+		
 	} else {
 		document.getElementById('infotext').innerHTML = "You can't do that move :)";
 	}
+}
+
 };	
