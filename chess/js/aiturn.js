ea6bb2543c6373f08a1a787848bd0dd79b52d448
Adrien <adrien.lafeuille@gmail.com>
2016-04-22 12:40:18 +0200

    Update aiturn.js

--- chess/js/aiturn.js
+++ chess/js/aiturn.js
@@ -1,5 +1,9 @@
 function aiturn()
 {
+	
+	
+	
+	
 	//load current baord in tmp variable for tests
 	var matrixai = new Array(8);
 	for(var i=0; i<8; i++) 
@@ -18,7 +22,7 @@ function aiturn()
 	
 	
 	//get legal moves for AI
-	listlegalmoves();
+	aifirstlist = listlegalmoves(matrix);
 	//loop and assess baord value after moving
 		startychoosen = -9999
 		startxchoosen = -9999
@@ -26,14 +30,14 @@ function aiturn()
 		stopxchoosen = -9999
 		bestboardfound = 9999
 
-	for (lilegalai = 0; lilegalai < legalmoveslist.length; lilegalai++) 
+	for (lilegalai = 0; lilegalai < aifirstlist.length; lilegalai++) 
 	{ 
 		//simulate move
-		legalmoveslist[lilegalai]
-		startyai = legalmoveslist[lilegalai][0]
-		startxai = legalmoveslist[lilegalai][1]
-		stopyai = legalmoveslist[lilegalai][2]
-		stopxai = legalmoveslist[lilegalai][3]
+		aifirstlist[lilegalai]
+		startyai = aifirstlist[lilegalai][0]
+		startxai = aifirstlist[lilegalai][1]
+		stopyai = aifirstlist[lilegalai][2]
+		stopxai = aifirstlist[lilegalai][3]
 		//copy the existing board
 			for(var myzeroli=0; myzeroli<8; myzeroli++) 
 			{
@@ -42,17 +46,78 @@ function aiturn()
 				matrixai[myzeroco][myzeroli] = matrix[myzeroco][myzeroli]
 			}
 			}
+		//simulate the move
 		matrixai[stopyai][stopxai] = matrixai[startyai][startxai]
 		matrixai[startyai][startxai] = 0
-		//assess board value -- you want to have boardvalue as low as possible
-		aftermoveboardvalue = (matrixai.reduce(function(a,b) { return a.concat(b) }).reduce(function(a,b) { return a + b })).toFixed(2)
-		if (aftermoveboardvalue < bestboardfound)
+		
+		//now test opponent reaction
+		///////////////////////////////////////////////////////////////////
+		whoisthinking = 1
+		playerreactionlist = listlegalmoves(matrixai);
+		startyhumananswer = -9999
+		startxhumananswer = -9999
+		stopyhumananswer = -9999
+		stopxhumananswer = -9999
+		strongesthumananswer = -9999
+		
+		var matrixaihuman = new Array(8);
+		for(var i=0; i<8; i++) 
+		{
+		matrixaihuman[i] = new Array(8);
+		}
+		//zero the entire board
+		for(var myzeroli=0; myzeroli<8; myzeroli++) 
+		{
+		for(var myzeroco=0; myzeroco<8; myzeroco++) 
+		{
+			matrixaihuman[myzeroco][myzeroli] = 0
+		}
+		}
+	
+
+		for (lilegalaihuman = 0; lilegalaihuman < playerreactionlist.length; lilegalaihuman++) 
 			{
-				startychoosen = startyai
-				startxchoosen = startxai
-				stopychoosen = stopyai
-				stopxchoosen = stopxai
+				//simulate move
+					playerreactionlist[lilegalaihuman]
+					startyaihuman = playerreactionlist[lilegalaihuman][0]
+					startxaihuman = playerreactionlist[lilegalaihuman][1]
+					stopyaihuman = playerreactionlist[lilegalaihuman][2]
+					stopxaihuman = playerreactionlist[lilegalaihuman][3]
+					//copy the existing board
+						for(var myzeroli=0; myzeroli<8; myzeroli++) 
+						{
+						for(var myzeroco=0; myzeroco<8; myzeroco++) 
+						{
+							matrixaihuman[myzeroco][myzeroli] = matrixai[myzeroco][myzeroli]
+						}
+						}
+					//simulate the move
+					matrixaihuman[stopyaihuman][stopxaihuman] = matrixaihuman[startyaihuman][startxaihuman]
+					matrixaihuman[startyaihuman][startxaihuman] = 0
+					//assess board value after AI and human played -- AI wants to have boardvalue as low as possible
+					aftermoveboardvalue = (matrixaihuman.reduce(function(a,b) { return a.concat(b) }).reduce(function(a,b) { return a + b })).toFixed(2)
+					if (aftermoveboardvalue > strongesthumananswer)
+						{
+							startychoosencandidate = startyai
+							startxchoosencandidate = startxai
+							stopychoosencandidate = stopyai
+							stopxchoosencandidate = stopxai
+							strongesthumananswer = aftermoveboardvalue
+						}
 			}
+			
+			if (strongesthumananswer<bestboardfound)
+			{
+				startychoosen = startychoosencandidate
+				startxchoosen = startxchoosencandidate
+				stopychoosen = stopychoosencandidate
+				stopxchoosen = stopxchoosencandidate
+				bestboardfound = strongesthumananswer
+			}
+			///////////////////////////////////////////////////////////////////
+		//go back to AI thinking status
+		whoisthinking = -1
+		
 	}
 
 	
