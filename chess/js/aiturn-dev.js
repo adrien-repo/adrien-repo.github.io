d2a0529bc6885d16105d8c233a218801d07a3eed
Adrien <adrien.lafeuille@gmail.com>
2016-04-22 21:19:35 +0200

    Update aiturn-dev.js

--- chess/js/aiturn-dev.js
+++ chess/js/aiturn-dev.js
@@ -1,183 +1,147 @@
 function aiturn()
 {
-	//creat blank matrix
-	var matrixai = new Array(8);
-	for(var i=0; i<8; i++) {matrixai[i] = new Array(8);}
-	for(var myzeroli=0; myzeroli<8; myzeroli++) {for(var myzeroco=0; myzeroco<8; myzeroco++) {matrixai[myzeroco][myzeroli] = 0}}
-	
-	
-	
-	//get legal moves for AI
-	aifirstlist = listlegalmoves(matrix);
-	aifirstlist = removechessfromlist(aifirstlist,matrix);
-	
-	//loop and assess baord value after moving
-		startychoosen = -9999
-		startxchoosen = -9999
-		stopychoosen = -9999
-		stopxchoosen = -9999
-		bestboardfound = 9999
-		strongesthumananswerever = 9999 //(tb minimized)
-		bestboardsecondai = 9999
-
-		
-		
-	for (lilegalai = 0; lilegalai < aifirstlist.length; lilegalai++) 
-	{ 
-		//simulate move
-		//aifirstlist[lilegalai]
-		startyai = aifirstlist[lilegalai][0]
-		startxai = aifirstlist[lilegalai][1]
-		stopyai = aifirstlist[lilegalai][2]
-		stopxai = aifirstlist[lilegalai][3]
-		//copy the existing board
-			for(var myzeroli=0; myzeroli<8; myzeroli++) 
-			{
-			for(var myzeroco=0; myzeroco<8; myzeroco++) 
-			{
-				matrixai[myzeroco][myzeroli] = matrix[myzeroco][myzeroli]
-			}
-			}
-		//simulate the move
-		matrixai[stopyai][stopxai] = matrixai[startyai][startxai]
-		matrixai[startyai][startxai] = 0
-		bestaifirst = (matrixai.reduce(function(a,b) { return a.concat(b) }).reduce(function(a,b) { return a + b })).toFixed(2)
-//		if ((bestaifirst < bestboardfound))
-//		{
-//			startychoosen = startyai
-//			startxchoosen = startxai
-//			stopychoosen = stopyai
-//			stopxchoosen = stopxai
-//			bestboardfound = bestaifirst
-			
-			
-			//now test opponent reaction
-			///////////////////////////////////////////////////////////////////
-			whoisthinking = 1
-			playerreactionlist = listlegalmoves(matrixai);
-			playerreactionlist = removechessfromlist(playerreactionlist,matrixai);
-			startyhumananswer = -9999
-			startxhumananswer = -9999
-			stopyhumananswer = -9999
-			stopxhumananswer = -9999
-			strongesthumananswer = -9999
-			
-			var matrixaihuman = new Array(8);
-			for(var i=0; i<8; i++) {matrixaihuman[i] = new Array(8);}
-			for(var myzeroli=0; myzeroli<8; myzeroli++) {for(var myzeroco=0; myzeroco<8; myzeroco++) {matrixaihuman[myzeroco][myzeroli] = 0	}}
+    //boxable loops for game discovery and rating
+    
+    //process description
+    //0 - for a given player color and matrix
+    //1 - list all legal moves + clear chess-resulting prohibited moves (external functions)
+    //2 - loop all admitted moves, get board rank <reloop here if needed>
+    //3 - rules for move choice 
+    //4 return best move coordinate for AI
+    
+    //init best move coordinates
+    startychoosen = -9999
+    startxchoosen = -9999
+    stopychoosen = -9999
+    stopxchoosen = -9999
+    
+    //init utility_treshold (tb minimized)
+    utility_treshold = 9999
+    
+    //assess current board 
+    evaluate_zero = (matrix.reduce(function(a,b) { return a.concat(b) }).reduce(function(a,b) { return a + b })).toFixed(2)
+    
+    //FIRST AI MOVE
+    //1.0 - for a given player color and matrix
+    whoisthinking = -1 
+    //we use matrix as start point
+    //1.1 - list all legal moves + clear chess-resulting prohibited moves (external fucntions)
+    list_first = listlegalmoves(matrix);
+    list_first = removechessfromlist(list_first,matrix);
+    //1.2 - loop all admitted moves, get board rank <reloop here if needed>
+    //creat blank matrix
+    var matrix_first = new Array(8);
+    for(var i=0; i<8; i++) {matrix_first[i] = new Array(8);}
+    for(var myzeroli=0; myzeroli<8; myzeroli++) {for(var myzeroco=0; myzeroco<8; myzeroco++) {matrix_first[myzeroco][myzeroli] = 0}}
+    //loop admitted moves
+    for (li_first = 0; li_first < list_first.length; li_first++) 
+        { 
+        //get 4 coordinates of move candidate
+        starty_first = list_first[li_first][0]
+        startx_first = list_first[li_first][1]
+        stopy_first = list_first[li_first][2]
+        stopx_first = list_first[li_first][3]
+        //set blank matrix to current board state
+            for(var myzeroli=0; myzeroli<8; myzeroli++)
+                {for(var myzeroco=0; myzeroco<8; myzeroco++) 
+                    {matrix_first[myzeroco][myzeroli] = matrix[myzeroco][myzeroli]}}
+        //simulate admitted move in mirrored matrix
+        matrix_first[stopy_first][stopx_first] = matrix_first[starty_first][startx_first]
+        matrix_first[starty_first][startx_first] = 0
+        //evaluate board after move
+        evaluate_first = (matrix_first.reduce(function(a,b) { return a.concat(b) }).reduce(function(a,b) { return a + b })).toFixed(2)
+        
 
 
-			for (lilegalaihuman = 0; lilegalaihuman < playerreactionlist.length; lilegalaihuman++) 
-				{
-					//simulate move
-						startyaihuman = playerreactionlist[lilegalaihuman][0]
-						startxaihuman = playerreactionlist[lilegalaihuman][1]
-						stopyaihuman = playerreactionlist[lilegalaihuman][2]
-						stopxaihuman = playerreactionlist[lilegalaihuman][3]
-						//copy the existing board
-							for(var myzeroli=0; myzeroli<8; myzeroli++) 
-							{
-							for(var myzeroco=0; myzeroco<8; myzeroco++) 
-							{
-								matrixaihuman[myzeroco][myzeroli] = matrixai[myzeroco][myzeroli]
-							}
-							}
-						//simulate the move: opponent always answers by the best move
-						matrixaihuman[stopyaihuman][stopxaihuman] = matrixaihuman[startyaihuman][startxaihuman]
-						matrixaihuman[startyaihuman][startxaihuman] = 0
-						humananswervalue = (matrixaihuman.reduce(function(a,b) { return a.concat(b) }).reduce(function(a,b) { return a + b })).toFixed(2)
-						if (humananswervalue >= strongesthumananswer)
-							{
-								strongesthumananswer = humananswervalue
-								if (strongesthumananswer < strongesthumananswerever)
-									{
-										strongesthumananswerever = strongesthumananswer
-									}
-								
-								///////////////////////////////////////////////////////////////////
-								//now test AI 2nd move
-								whoisthinking = -1
-								aisecondlist = listlegalmoves(matrixaihuman);
-								aisecondlist = removechessfromlist(aisecondlist,matrixaihuman);
-								startysecondai = -9999
-								startxsecondai = -9999
-								stopysecondai = -9999
-								stopxsecondai = -9999
-
-								var matrixsecondai = new Array(8);
-								for(var i=0; i<8; i++) {matrixsecondai[i] = new Array(8);}
-								for(var myzeroli=0; myzeroli<8; myzeroli++) {for(var myzeroco=0; myzeroco<8; myzeroco++) {matrixsecondai[myzeroco][myzeroli] = 0}}
-								for (lilegalaisecond = 0; lilegalaisecond < aisecondlist.length; lilegalaisecond++) 
-								{
-									//simulate move
-									startysecondai = aisecondlist[lilegalaisecond][0]
-									startxsecondai = aisecondlist[lilegalaisecond][1]
-									stopysecondai = aisecondlist[lilegalaisecond][2]
-									stopxsecondai = aisecondlist[lilegalaisecond][3]
-									//copy the existing board
-									for(var myzeroli=0; myzeroli<8; myzeroli++) 
-									{
-									for(var myzeroco=0; myzeroco<8; myzeroco++) 
-									{
-										matrixsecondai[myzeroco][myzeroli] = matrixaihuman[myzeroco][myzeroli]
-									}
-									}
-									//simulate the move
-									matrixsecondai[stopysecondai][stopxsecondai] = matrixsecondai[startysecondai][startxsecondai]
-									matrixsecondai[startysecondai][startxsecondai] = 0
-									//assess board value after AI and human played, then AI answered -- AI wants to have boardvalue as low as possible
-									bestaisecond = (matrixsecondai.reduce(function(a,b) { return a.concat(b) }).reduce(function(a,b) { return a + b })).toFixed(2)
-										
-										
-										//we have 3 rules for AI
-										//1 - by default, play the best next move, reset next move treshold
-										
-												if ((bestaifirst < bestboardfound))
-												{
-													startychoosen = startyai
-													startxchoosen = startxai
-													stopychoosen = stopyai
-													stopxchoosen = stopxai
-													bestboardfound = bestaifirst
-												}
-										//2 - when two moves are competing, play the one that minimize opponent next move impact
-												if ((bestaifirst == bestboardfound) && (strongesthumananswer<bestaifirst))
-												{
-													startychoosen = startyai
-													startxchoosen = startxai
-													stopychoosen = stopyai
-													stopxchoosen = stopxai
-												}
-										//3 - a non weakest opponent next move is tolerated if in two moves, you are better off than after the first
-												if ((bestaisecond < bestboardfound) && (bestaisecond < bestboardsecondai))
-												{
-													startychoosen = startyai
-													startxchoosen = startxai
-													stopychoosen = stopyai
-													stopxchoosen = stopxai
-													bestboardsecondai = bestaisecond
-												}
-												
-										
-								
-								}
-								///////////////////////////////////////////////////////////////////
-							}		
-
-				}
-				
-//		}
-	}
-			///////////////////////////////////////////////////////////////////
-		//go back to AI thinking status
-		whoisthinking = -1
-		
-	
-
-	
+    //SECOND HUMAN MOVE
+    //2.0 - for a given player color and matrix
+    whoisthinking = 1
+    //we use matrix_first as start point
+    //2.1 - list all legal moves + clear chess-resulting prohibited moves (external fucntions)
+    list_second = listlegalmoves(matrix_first);
+    list_second = removechessfromlist(list_second,matrix_first);
+    //2.2 - loop all admitted moves, get board rank <reloop here if needed>
+    //creat blank matrix
+    var matrix_second = new Array(8);
+    for(var i=0; i<8; i++) {matrix_second[i] = new Array(8);}
+    for(var myzeroli=0; myzeroli<8; myzeroli++) {for(var myzeroco=0; myzeroco<8; myzeroco++) {matrix_second[myzeroco][myzeroli] = 0}}
+    //loop admitted moves
+    for (li_second = 0; li_second < list_second.length; li_second++) 
+        { 
+        //get 4 coordinates of move candidate
+        starty_second = list_second[li_second][0]
+        startx_second = list_second[li_second][1]
+        stopy_second = list_second[li_second][2]
+        stopx_second = list_second[li_second][3]
+        //set blank matrix to current board state
+            for(var myzeroli=0; myzeroli<8; myzeroli++)
+                {for(var myzeroco=0; myzeroco<8; myzeroco++) 
+                    {matrix_second[myzeroco][myzeroli] = matrix_first[myzeroco][myzeroli]}}
+        //simulate admitted move in mirrored matrix
+        matrix_second[stopy_second][stopx_second] = matrix_second[starty_second][startx_second]
+        matrix_second[starty_second][startx_second] = 0
+        //evaluate board after move
+        evaluate_second = (matrix_second.reduce(function(a,b) { return a.concat(b) }).reduce(function(a,b) { return a + b })).toFixed(2)
 
 
-	
 
+    //THIRD AI MOVE
+    //3.0 - for a given player color and matrix
+    whoisthinking = -1
+    //we use matrix_second as start point
+    //3.1 - list all legal moves + clear chess-resulting prohibited moves (external fucntions)
+    list_third = listlegalmoves(matrix_second);
+    list_third = removechessfromlist(list_third,matrix_second);
+    //2.2 - loop all admitted moves, get board rank <reloop here if needed>
+    //creat blank matrix
+    var matrix_third = new Array(8);
+    for(var i=0; i<8; i++) {matrix_third[i] = new Array(8);}
+    for(var myzeroli=0; myzeroli<8; myzeroli++) {for(var myzeroco=0; myzeroco<8; myzeroco++) {matrix_third[myzeroco][myzeroli] = 0}}
+    //loop admitted moves
+    for (li_third = 0; li_third < list_third.length; li_second++) 
+        { 
+        //get 4 coordinates of move candidate
+        starty_third = list_third[li_third][0]
+        startx_third = list_third[li_third][1]
+        stopy_third = list_third[li_third][2]
+        stopx_third = list_third[li_third][3]
+        //set blank matrix to current board state
+            for(var myzeroli=0; myzeroli<8; myzeroli++)
+                {for(var myzeroco=0; myzeroco<8; myzeroco++) 
+                    {matrix_third[myzeroco][myzeroli] = matrix_second[myzeroco][myzeroli]}}
+        //simulate admitted move in mirrored matrix
+        matrix_third[stopy_third][stopx_third] = matrix_third[starty_third][startx_third]
+        matrix_third[starty_third][startx_third] = 0
+        //evaluate board after move
+        evaluate_third = (matrix_third.reduce(function(a,b) { return a.concat(b) }).reduce(function(a,b) { return a + b })).toFixed(2)
 
-};
+        utility_first = evaluate_first - evaluate_zero
+        utility_second = evaluate_second - evaluate_zero
+        utility_third = evaluate_third - evaluate_zero
+        
+        utility_function = utility_first - utility_second + utility_third
+        //keep the move that minimizes the utility_function
+        if (utility_function < utility_treshold)
+        {
+        startychoosen = starty_first
+        startxchoosen = startx_first
+        stopychoosen = stopy_first
+        stopxchoosen = stopx_first
+        }
+
+        //end of third loop
+        }
+
+        //end of second loop
+        }
+
+        //end of first loop
+        }
+
+       
+       
+       
+       
+       
+//end of function
+}        
